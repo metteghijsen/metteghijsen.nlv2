e9565f4dfd51db66a2e579d94b41e2fd64756534
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2024-06-19 01:28:21 +0200

    Feature: Added small move up when hovering on a project

--- frontend/components/ui/Project.vue
+++ frontend/components/ui/Project.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink :to="to">
-    <div class="movingBackground background z-0 lg:h-[700px] md:max-w-[680px] w-full flex items-center justify-center rounded-3xl">
+    <div class="movingBackground background z-0 lg:h-[700px] md:max-w-[680px] w-full flex items-center justify-center rounded-3xl hover:translate-y-[-5px] ease-in-out transition-transform">
       <div class="z-10 bg-white100 opacity-90 dark:bg-[#020520] dark:border-slate-600 border-2 lg:h-[696px] md:max-w-[676px] flex flex-col items-center rounded-3xl dark:hover:border-blue100 hover:border-blue100 transition-all ease-in-out duration-150">
         <NuxtImg :src="`/img/projects/${imageName}.png`" :alt="imageName" class="object-cover aspect-video w-full max-h-[400px] mb-4 rounded-t-3xl" />
         <div class="flex flex-col mx-10">
