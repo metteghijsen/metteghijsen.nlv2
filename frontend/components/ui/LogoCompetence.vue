9763c8b9edc3b7efefe6502d17895db72c9c804c
Mette Ghijsen <mette.ghijsen@student.fontys.nl>
2024-06-19 15:39:34 +0200

    Feature: Added hover effect to logo component

--- frontend/components/ui/LogoCompetence.vue
+++ frontend/components/ui/LogoCompetence.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex justify-center items-center hover:translate-y-[-5px] transition-all ease-in-out">
     <NuxtImg :src="`/img/logos/competences/${imageName}.png`" :alt="imageName" class="lg:max-h-[75px] object-cover max-h-[50px] m-4" />
   </div>
 </template>
