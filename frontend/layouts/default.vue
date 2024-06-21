d542b0b634359c992497723384318b11a1b9de7b
I481695 <mette.ghijsen@student.fontys.nl>
2024-06-22 01:58:37 +0200

    Feature: Added a small easteregg on each webpage

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,4 +2,19 @@
   <div class="m-0 overflow-x-hidden">
     <slot />
   </div>
+  <!--
+
+  __  __      _   _          _____ _     _ _
+ |  \/  |    | | | |        / ____| |   (_|_)
+ | \  / | ___| |_| |_ ___  | |  __| |__  _ _ ___  ___ _ __
+ | |\/| |/ _ \ __| __/ _ \ | | |_ | '_ \| | / __|/ _ \ '_ \
+ | |  | |  __/ |_| ||  __/ | |__| | | | | | \__ \  __/ | | |
+ |_|  |_|\___|\__|\__\___|  \_____|_| |_|_| |___/\___|_| |_|
+                                         _/ |
+                                        |__/
+Wat leuk dat je op mijn site kijkt!
+Interesse? De site is te vinden op mijn GitHub :)
+Als je vragen hebt, neem vooral contact met me op. Ik help je graag!
+
+-->
 </template>
