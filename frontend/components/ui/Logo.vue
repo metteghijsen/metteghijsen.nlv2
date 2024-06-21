27e7065f147c367e2435475f7b1c15fa28698649
I481695 <mette.ghijsen@student.fontys.nl>
2024-06-22 01:58:24 +0200

    Feature: Created a more logical file structure

--- frontend/components/ui/Logo.vue
+++ frontend/components/ui/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center">
     <NuxtLink :to="to" target="_blank">
-      <NuxtImg :src="`/img/logos/${imageName}.png`" :alt="imageName" :class="[invert ? 'dark:invert' : '' , keepInverted ? '' : 'dark:hover:invert-0']" class="dark:grayscale dark:hover:grayscale-0 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out lg:max-h-[100px] object-cover sm:max-h-[100px]" />
+      <NuxtImg :src="`/img/logos/partners/${imageName}.png`" :alt="imageName" :class="[invert ? 'dark:invert' : '' , keepInverted ? '' : 'dark:hover:invert-0']" class="dark:grayscale dark:hover:grayscale-0 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out lg:max-h-[100px] object-cover sm:max-h-[100px]" />
     </NuxtLink>
   </div>
 </template>
