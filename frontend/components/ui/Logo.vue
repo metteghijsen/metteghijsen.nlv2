43bbfe86a1c0187fddceb28a0e7f246277ff844c
I481695 <mette.ghijsen@student.fontys.nl>
2024-06-22 01:36:28 +0200

    Feature: Added adaptive classes to make certain logos look more appealing

--- frontend/components/ui/Logo.vue
+++ frontend/components/ui/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center">
     <NuxtLink :to="to" target="_blank">
-      <NuxtImg :src="`/img/logos/${imageName}.png`" :alt="imageName" class="dark:invert dark:grayscale dark:hover:invert-0 dark:hover:grayscale-0 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out lg:max-h-[100px] object-cover sm:max-h-[100px]" />
+      <NuxtImg :src="`/img/logos/${imageName}.png`" :alt="imageName" :class="[invert ? 'dark:invert' : '' , keepInverted ? '' : 'dark:hover:invert-0']" class="dark:grayscale dark:hover:grayscale-0 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out lg:max-h-[100px] object-cover sm:max-h-[100px]" />
     </NuxtLink>
   </div>
 </template>
@@ -16,6 +16,14 @@ export default defineComponent({
     },
     to: {
       type: String,
+      required: true
+    },
+    invert: {
+      type: Boolean,
+      required: true
+    },
+    keepInverted: {
+      type: Boolean,
       required: true
     }
   }
