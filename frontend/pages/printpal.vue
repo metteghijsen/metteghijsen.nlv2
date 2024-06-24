2b832fcd290df151a64fdc920f741260e7273e14
I481695 <mette.ghijsen@student.fontys.nl>
2024-06-25 01:01:59 +0200

    Docs: Removed wrong placed quotation marks

--- frontend/pages/printpal.vue
+++ frontend/pages/printpal.vue
@@ -12,7 +12,6 @@
         Printpal is an application that supports people with depression by sending positive messages to a pocket printer. Within the app you get the ability to send prints to your friends and random people. You can also choose what kind of prints you want to receive at what time. If you are not near your printer, for example because you are traveling and did not bring it with you, the app offers you the possibility to view these prints anywhere, anytime! You can also chat with the people who have chosen to receive a response to their prints. This way you can talk together about sensitive topics, and get a glimpse into the mind of someone who is experiencing the same thing as you. By doing so, we hope to encourage people to start the conversation, and make new friends.
         <br><br>
         I made this app together with my project group in the second year of my study ICT & Media Design (2022). We participated in the ProjectB competition of the company Sogeti. The assignment of the competition was: Create an IT innovation for people with a (mental) disorder or chronic illness. I pitched this idea together with a fellow group member in front of 120 people. Unfortunately, we did not win anything.
-        "
       </UiTextbox>
       <NuxtImg v-motion-slide-visible-once-bottom src="/img/projectimage3.png" class="w-full" />
 
