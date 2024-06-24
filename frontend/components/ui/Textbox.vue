ae83f5f0e54e28a7689ca138270253e630c85613
I481695 <mette.ghijsen@student.fontys.nl>
2024-06-25 00:34:25 +0200

    Fix: Responsiveness issues

--- frontend/components/ui/Textbox.vue
+++ frontend/components/ui/Textbox.vue
@@ -1,15 +1,14 @@
 <template>
   <div
-    :class="[darkModeClass, 'gradient-background-light']"
-    class="pt-14 pb-14 flex justify-center"
+    class="flex justify-center items-center bg-white dark:bg-[#020520] "
   >
-    <div class="w-full py-9 md:px-[250px] md:grid md:grid-cols-2 flex flex-col bg-white dark:bg-[#020520] border-y-slate-200 dark:border-y-slate-600 border-y-[1px]">
-      <div class="flex justify-center max-w-[700px] mt-7 cursor-default lg:text-[70px] md:text-[45px] text-[30px] poppins break-words whitespace-normal text-black dark:text-white">
+    <div v-motion-slide-visible-once-bottom class="w-full py-9 xl:px-[250px] xl:grid xl:grid-cols-2 flex justify-center flex-col items-center lg:items-start px-4 cursor-default">
+      <div class="w-full flex flex-row justify-start lg:max-w-[600px] mt-7 cursor-default lg:text-[70px] md:text-[45px] text-[30px] poppins break-words whitespace-normal text-black dark:text-white">
         <h1 v-motion-slide-bottom class="dark:custom-hover-darkmode transition-all ease-in-out">
           {{ titleText }}
         </h1>
       </div>
-      <div class="flex justify-center max-w-[700px]">
+      <div class="flex justify-center md:max-w-[700px] w-full">
         <UiTypography v-motion-slide-bottom type="p" size="paragraph-small" class="text-left py-3">
           <slot />
         </UiTypography>
