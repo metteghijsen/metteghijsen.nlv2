3c781d298675f28ebd5a40de5c54fd5627ad3510
I481695 <mette.ghijsen@student.fontys.nl>
2025-02-12 20:50:49 +0100

    Fix: Build problemen opgelost

--- frontend/pages/playground/mette.vue
+++ frontend/pages/playground/mette.vue
@@ -103,56 +103,7 @@
             <UiButton label="leeswijzer" secondary big />
           </div>
         </div>
-
-        <div class="flex justify-center">
-          <UiDeliverable
-            lo4
-            lo7
-            to="/"
-            deliverable-name="Projectplan"
-            image-name="projectplan"
-            deliverable-description="In dit document beschrijf ik mijn Personal Project Plan en geef ik een overzicht van het proces dat ik heb doorlopen bij het opstellen van dit plan."
-            project-type="Personal Project"
-          />
-
-          <UiDeliverable
-            lo1
-            lo3
-            to="/"
-            deliverable-name="Library Research"
-            image-name="libraryresearch"
-            deliverable-description="In dit document beschrijf ik mijn Personal Project Plan en geef ik een overzicht van het proces dat ik heb doorlopen bij het opstellen van dit plan."
-            project-type="Personal Project"
-          />
-
-          <UiDeliverable
-            lo2
-            lo3
-            lo6
-            to="/"
-            deliverable-name="Wireframing"
-            image-name="wireframing"
-            deliverable-description="In dit document beschrijf ik mijn Personal Project Plan en geef ik een overzicht van het proces dat ik heb doorlopen bij het opstellen van dit plan."
-            project-type="Personal Project"
-          />
-        </div>
       </div>
-
-      <div class="flex flex-row">
-        <div class="flex flex-col">
-          <UiLearningOutcome lo-name-color="text-red100" lo-name="1. User Interaction (Analysis & Advice)" lo-description="You analyse the user, the user interaction and the user experience, also taking state of the art interactive technologies into account. You select a suitable design process to be able to advise on UX interventions based on a validated UX design." />
-          <UiLearningOutcome lo-name-color="text-yellow100" lo-name="3. Software design and realisation" lo-description="You create & design software with existing components or libraries using predetermined quality criteria and version control." />
-          <UiLearningOutcome lo-name-color="text-blue100" lo-name="5. Investigative problem solving" lo-description="You formulate sub-questions pertaining to the primary question and answer these using relevant research methods. You use the conclusions of the sub-questions to justify (design) choices." />
-        </div>
-
-        <div class="flex flex-col">
-          <UiLearningOutcome lo-name-color="text-orange100" lo-name="2. User Interaction (Execution & Validation)" lo-description="You execute and evaluate the user experience of an interactive product. You document the development process for the stakeholders." />
-          <UiLearningOutcome lo-name-color="text-green100" lo-name="4. Future-oriented organisation" lo-description="You carry out a problem analysis and on that basis, you determine the definitive problem and elaborate on this in a project plan." />
-        </div>
-      </div>
-
-      <UiProject image-name="developmentwebsite" project-name="Individueel Project" project-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate tristique felis, non malesuada sapien fermentum id. Sed commodo diam in velit vestibulum, vel feugiat erat scelerisque. Suspendisse potenti. Aenean sit amet bibendum nunc. Sed auctor justo id mi eleifend, nec dictum metus convallis. In hac habitasse platea dictumst. Etiam ac risus ut urna rhoncus efficitur. " />
-      <UiProject image-right image-name="libraryresearch" project-name="Groepsproject" project-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate tristique felis, non malesuada sapien fermentum id. Sed commodo diam in velit vestibulum, vel feugiat erat scelerisque. Suspendisse potenti. Aenean sit amet bibendum nunc. Sed auctor justo id mi eleifend, nec dictum metus convallis. In hac habitasse platea dictumst. Etiam ac risus ut urna rhoncus efficitur. " />
     </UiContainer>
     <SectionsFooter />
   </div>
